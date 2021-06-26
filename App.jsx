759c2d8e3060cb92c62c03d8cf24b417940ec5c2
Uday-7046 <85827280+Uday-7046@users.noreply.github.com>
2021-06-26 19:33:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Node";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
